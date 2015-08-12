0021e8db0165b976be7278d1e1fa4f703318bc15
Wellington <wellington.braga@yahoo.com.br>
2015-08-12 14:41:00 -0300

    Criação do javascript

--- src/prodam/tiles/static/tiles.js
+++ src/prodam/tiles/static/tiles.js
@@ -7,3 +7,18 @@ $(function() {
     $(window).load(function(){
     });
 });
+
+(function($) {
+   $(document).ready(function() {
+    $("#controler-carrossel a").bind("click", function(){
+        event.preventDefault();
+        $('.ativo').each(function(){
+            $(this).removeClass('ativo');
+        })
+        var controle = '.' + $(this).attr('class');
+        $(controle).each(function() {
+            $(this).addClass('ativo')
+        });
+    })
+   })
+})(jQuery);

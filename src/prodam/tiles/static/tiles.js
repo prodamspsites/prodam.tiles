1c5131f4a0efe97362c83d3c3d606c6cbb386879
Wellington <wellington.braga@yahoo.com.br>
2015-08-17 09:33:11 -0300

    Estiliza e javascript do banner de seções

--- src/prodam/tiles/static/tiles.js
+++ src/prodam/tiles/static/tiles.js
@@ -11,7 +11,7 @@ $(function() {
 (function($) {
    $(document).ready(function() {
 
-    $("#controler-carrossel a").bind("click", function(){
+    $(".section-prefeitura-de-sao-paulo #controler-carrossel a").bind("click", function(){
         event.preventDefault();
         $('.ativo').each(function(){
             $(this).removeClass('ativo');
@@ -21,6 +21,21 @@ $(function() {
             $(this).addClass('ativo')
         });
         $('.ativo').css({
+            opacity: 0.8
+        }).animate({
+            opacity:1
+        },250,'easeInSine')
+    })
+
+    $(".bannerInfo h2 a").bind("click", function(){
+        event.preventDefault();
+        thisParent = $(this).parent()
+        bannerInfo = $(thisParent).parent()
+        $('div.chamada-item img, .bannerInfo p').each(function(){
+            $(this).hide();
+        })
+        var controle = 'div.' + $(thisParent).attr('class');
+        $(controle + ' img, ' + controle + ' p').show().css({
             opacity: 0.8
         }).animate({
             opacity:1

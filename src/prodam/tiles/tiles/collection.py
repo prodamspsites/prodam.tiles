e57a537d55078bf8ef7dd4a29a8d168d6ee16f54
mosesjr <shank.sa@gmail.com>
2015-08-19 10:14:09 -0300

    Adiciona campo de link do footer no tile collection

--- src/prodam/tiles/tiles/collection.py
+++ src/prodam/tiles/tiles/collection.py
@@ -3,14 +3,26 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from collective.cover.tiles.collection import CollectionTile
 from collective.cover.tiles.collection import ICollectionTile
+from prodam.tiles import _
+from zope import schema
 
 
 class ICollectionTile(ICollectionTile):
-    pass
+
+    footer_link = schema.TextLine(
+        title=_(u'Footer link'),
+        required=False,
+    )
 
 
 class CollectionTile(CollectionTile):
     index = ViewPageTemplateFile('templates/collection.pt')
+
+    def getFooterLink(self):
+        if self.data.get('footer_link', None):
+            return self.data.get('footer_link', None)
+        else:
+            return False
 
     def hasImage(self, item):
         if item.getObject().image:
